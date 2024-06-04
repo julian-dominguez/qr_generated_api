91dcca1ef670d2cda74f13bccdff233cd63169ae
Julian Dominguez <juliand.dominguezc@gmail.com>
2024-06-04 16:09:31 -0500

    Se añade event listener para el manejo de las excepciones para que estas siempre sean retornadas con el mismo formato json

---
+++ src/EvenListener/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EvenListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+#[AsEventListener]
+class ExceptionListener
+{
+
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $response = new JsonResponse([
+            'error' => $exception->getMessage(),
+        ]);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+        } else {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+
+}
