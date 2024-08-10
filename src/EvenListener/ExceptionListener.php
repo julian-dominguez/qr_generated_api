5f67a6feac98074f106ca069678f7c0f9ed2da3a
Julian Dominguez <juliand.dominguezc@gmail.com>
2024-08-10 12:08:45 -0500

    Se mejora el ExceptionListener.php para lograr tener una página de inicio con la información general de la API y los accesos a la documentación

--- src/EvenListener/ExceptionListener.php
+++ src/EvenListener/ExceptionListener.php
@@ -6,15 +6,48 @@ namespace App\EvenListener;
 
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 #[AsEventListener]
 class ExceptionListener
 {
+    private Environment $twig;
 
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function __invoke(ExceptionEvent $event): void
     {
+        $request = $event->getRequest();
+        $path = $request->getPathInfo();
+
+        // Si el path es la página raíz, renderizar la vista Twig
+        if ($path === '/') {
+            $content = $this->twig->render('index_api/index.html.twig');
+            $event->setResponse(
+                new Response(
+                    $content,
+                    Response::HTTP_OK,
+                    ['Content-Type' => 'text/html']
+                )
+            );
+
+            return;
+        }
+
         $exception = $event->getThrowable();
         $response = new JsonResponse([
             'error' => $exception->getMessage(),
@@ -28,5 +61,4 @@ class ExceptionListener
 
         $event->setResponse($response);
     }
-
 }
